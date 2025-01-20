92be03dc313649541b0539ee26e9c46d2aebb971
Edanur Korkmaz <edanuurkorkmaz@gmail.com>
2025-01-20 21:16:37 +0300

    ödev tamamlandı.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,65 +6,42 @@ if (localStorage.tasks) {
     tasks = JSON.parse(localStorage.tasks);
 }
 
-taskSaveBtn.addEventListener("click",function(){
-    console.log(taskInput.value);
-
-    let newId = 1;
-    if (tasks[tasks.length - 1]) {
-        newId = tasks[tasks.length - 1].id + 1;
-    }
-
-    const newTask ={
-        id: newId,
-        title: taskInput.value,
-        isCompleted: false,
-    };
-    
-    tasks.push(newTask);
-
-    localStorage.tasks = JSON.stringify(tasks);
-
-    taskInput.value = "";
-
-    render();
-});
-
 function render(type = "uncompleted"){
     taskList.innerHTML = "";
    
     if (type === "uncompleted") {
-        showUnCompleted.style.background = "#638CB6";
-        showCompleted.style.background = "transparent";
+        showUnCompleted.style.background = "#6741D9";
+        showCompleted.style.background = "#fff";
         const filteredTasks = tasks.filter(task => !task.isCompleted);
         if (filteredTasks.length === 0) {
-            taskList.innerHTML = "Tamamlanmayan görev yok";
+            taskList.innerHTML = `<p class = "task-text">Tamamlanmayan görev yok</p>`;
             return;
         }
     
         for (const task of filteredTasks) {
             taskList.innerHTML += `
-            <li>
-            <span contenteditable = "true">${task.title} </span>
+            <li class = "taskItem">
+            <button  data-id = "${task.id}" class = "btn markCompleteBtn"></button>
+            <span data-id = "${task.id}" contenteditable = "true" class="taskTitle">${task.title} </span>
             <!-- <i data-id = "${task.id}" class = "fas fa-pencil editTaskBtn"></i> -->
             <i data-id = "${task.id}" class = "fas fa-trash deleteTaskBtn"></i>
-            <i  data-id = "${task.id}" class = "fas fa-check markCompleteBtn"></i>
             </li>`;
         }
         
     }else{
         taskList.innerHTML = "";
-        showCompleted.style.background = "#638CB6";
-        showUnCompleted.style.background = "transparent";
+        showCompleted.style.background = "#6741D9";
+        showUnCompleted.style.background = "#fff";
         const filteredTasks = tasks.filter(task => task.isCompleted);
         if (filteredTasks.length === 0) {
-            taskList.innerHTML = "Hiç tamamlanmış görev yok";
+            taskList.innerHTML = `<p class = "task-text">Hiç tamamlanmış görev yok</p>`;
             return;
         }
     
         for (const task of filteredTasks) {
             taskList.innerHTML += `
-            <li>
-            ${task.title} 
+            <li class = "taskItem">
+             <span data-id = "${task.id}" contenteditable = "true" class="taskTitle">${task.title} </span>
            <i  data-id = "${task.id}" id= "unMarkCompleteBtn" class = "fas fa-circle-xmark markCompleteBtn "></i>
             </li>`;
         }
@@ -89,6 +66,16 @@ function bindElements(){
     // const editTaskBtns = document.querySelectorAll(".editTaskBtn");
     const deleteTaskBtns = document.querySelectorAll(".deleteTaskBtn");
     // const markUnCompleteBtns = document.querySelectorAll(".markUnCompleteBtn");
+    const taskTitles = document.querySelectorAll(".taskTitle");
+    for (const taskTitle of taskTitles) {
+        taskTitle.addEventListener("focusout", function (e){
+            findedTask = tasks.find(task => task.id == e.target.dataset.id);
+            findedTask.title = e.target.textContent;
+            localStorage.tasks = JSON.stringify(tasks);
+            
+        })
+        
+    }
     for (const markCompleteBtn of markCompleteBtns) {
         markCompleteBtn.addEventListener("click", function(e){
             
@@ -159,3 +146,39 @@ function deleteTask(id){
     }
     
 }
+newTaskBtn.addEventListener("click",function(e){
+    newTaskBtn.style.display = "none";
+    newTaskForm.style.display = "block";
+    newTaskInput.focus();
+
+});
+ newTaskForm.addEventListener("submit", function(e){
+        e.preventDefault();
+        console.log(e.target);
+        // const formData = new FormData(e.target);
+        // const formObj = Object.fromEntries(formData);
+        
+        const formObj = Object.fromEntries(new FormData(e.target));
+        console.log(formObj);
+        
+        let newId = 1;
+        if (tasks[tasks.length - 1]) {
+            newId = tasks[tasks.length - 1].id + 1;
+        }
+
+        const newTask ={
+            id: newId,
+            title: formObj.task,
+            isCompleted: false,
+        };
+        
+        tasks.push(newTask);
+
+    localStorage.tasks = JSON.stringify(tasks);
+        
+        e.target.reset();
+        newTaskBtn.style.display = "block";
+        newTaskForm.style.display = "none";
+
+        render();
+    });
